8be581b1465e2e3804f1fb8e20bd18f6973adce2
Alina <maliniaka.web@gmail.com>
2018-03-17 20:26:16 +0100

    Lesson 4

---
+++ php-HW04.php
@@ -0,0 +1,44 @@
+<?php 
+
+function greeting($name) {
+	echo "Привет $name! ";
+	$today = getdate();
+	if ( $today[wday] < 6 ) {
+		echo "Хорошего и продуктивного рабочего дня!";
+	} else {
+		echo "Желаю вам хорошо отдохнуть в этот день!";
+	}	
+}
+
+greeting("Alina");
+
+echo "<br><br><i>greeting_second('Alina', 1)</i>:<br><br>";
+
+function greeting_second($name, $day) {
+	echo "Привет $name! ";
+	switch ($day) {
+		case 1:
+			# code...
+		case 2:
+			# code...
+		case 3:
+			# code...
+		case 4:
+			# code...
+		case 5:
+			# code...
+			echo "Хорошего и продуктивного рабочего дня!";
+			break;
+		case 6:
+			# code...
+		case 7:
+			# code...
+		default:
+			echo "Желаю вам хорошо отдохнуть в этот день!";
+			break;
+	}
+}
+
+greeting_second("Alina", 1);
+
+?>
