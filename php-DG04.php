1ad2bdc5c97aa5e57fba6786c64bda1cecf8fae7
DimiG <dim.georgiev1976@yandex.com>
2018-03-23 18:39:00 +0800

    Secoun fixed greeting #6

--- php-DG04.php
+++ php-DG04.php
@@ -1,34 +1,22 @@
 		<?php
 
 function greeting( $name = "Гость", $dayNumber = "день") {
-			$name = 'Димитар';
-			$dayNumber = 4;
-switch ( $dayNumber ) {
-	case '1':
-	case '2':	
-	case '3':
-	case '4':
-	case '5':
-	echo "Привет, $name! Хорошего и продуктивного рабочего день!";
-		break;
-	case '6':
-	case '7':
-	echo "Привет, $name! Желаю вам хорошо отдохнуть в этот день!";
-		break;
-	default:
-	echo "Привет, $name! Видимо у вас неполадки с календарем!";
-		break;
-}
+	switch ( $dayNumber ) {
+		case '1': case '2':	case '3': case '4': case '5':
+			echo "Привет, $name! Хорошего и продуктивного рабочего день!";
+			break;
+		case '6': case '7':
+			echo "Привет, $name! Желаю вам хорошо отдохнуть в этот день!";
+			break;
+		default:
+			echo "Привет, $name! Видимо у вас неполадки с календарем!";
+			break;
+		}
 	}
-echo "case: day is 1 - 5 <br>";
-greeting();
+greeting('Димитар', 4);
 echo '<br><br>';
-
-echo "case: day is 6 - 7 <br>";
-greeting();
+greeting("Димитар", 7);
 echo '<br><br>';
-
-echo "case: day is out of 1 - 7 or absent <br>";
-greeting();
+greeting("Димитар", 8);
 echo '<br><br>';
 ?>
